822433ec0a5bef47de82a1b6a7cc8a2fa0c08c24
Lingxuan Ye <ink@lingxuan.io>
2026-06-25 20:27:18 +0800

    refactor: improve readability

--- src/vec/iter.rs
+++ src/vec/iter.rs
@@ -1,7 +1,9 @@
 use super::InlineVec;
+use crate::buf::Buf;
 use core::fmt;
 use core::iter::FusedIterator;
 use core::mem::ManuallyDrop;
+use core::range::Range;
 use core::slice::{Iter, IterMut};
 
 impl<'a, T, const N: usize> IntoIterator for &'a InlineVec<T, N> {
@@ -27,15 +29,47 @@ impl<T, const N: usize> IntoIterator for InlineVec<T, N> {
     type IntoIter = IntoIter<T, N>;
 
     fn into_iter(self) -> Self::IntoIter {
-        let index = 0;
-        let buf = ManuallyDrop::new(self);
-        IntoIter { index, buf }
+        let start = 0;
+        let end_and_buf = ManuallyDrop::new(self);
+        IntoIter { start, end_and_buf }
     }
 }
 
 pub struct IntoIter<T, const N: usize> {
-    index: usize,
-    buf: ManuallyDrop<InlineVec<T, N>>,
+    start: usize,
+    end_and_buf: ManuallyDrop<InlineVec<T, N>>,
+}
+
+impl<T, const N: usize> IntoIter<T, N> {
+    fn alive(&self) -> Range<usize> {
+        let start = self.start();
+        let end = self.end();
+        Range { start, end }
+    }
+
+    fn start(&self) -> usize {
+        self.start
+    }
+
+    unsafe fn start_mut(&mut self) -> &mut usize {
+        &mut self.start
+    }
+
+    fn end(&self) -> usize {
+        self.end_and_buf.len
+    }
+
+    unsafe fn end_mut(&mut self) -> &mut usize {
+        &mut self.end_and_buf.len
+    }
+
+    fn buf(&self) -> &Buf<T, N> {
+        &self.end_and_buf.buf
+    }
+
+    fn buf_mut(&mut self) -> &mut Buf<T, N> {
+        &mut self.end_and_buf.buf
+    }
 }
 
 impl<T, const N: usize> fmt::Debug for IntoIter<T, N>
@@ -43,12 +77,7 @@ where
     T: fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let slice = unsafe {
-            self.buf
-                .buf
-                .get_unchecked(self.index..self.buf.len)
-                .assume_init_ref()
-        };
+        let slice = unsafe { self.buf().get_unchecked(self.alive()).assume_init_ref() };
         f.debug_list().entries(slice).finish()
     }
 }
@@ -58,17 +87,20 @@ where
     T: Clone,
 {
     fn clone(&self) -> Self {
-        let index = self.index;
-        let buf = ManuallyDrop::new(InlineVec::new());
-        let mut iter = Self { index, buf };
-        iter.buf.len = self.index;
-        while iter.buf.len != self.buf.len {
-            let index = iter.buf.len;
+        let start = 0;
+        let end_and_buf = ManuallyDrop::new(InlineVec::new());
+        let mut iter = Self { start, end_and_buf };
+        unsafe {
+            *iter.start_mut() = self.start();
+            *iter.end_mut() = self.start();
+        }
+        while iter.end() != self.end() {
+            let index = iter.end();
             unsafe {
-                let value = self.buf.buf.assume_init_clone(index);
-                iter.buf.buf.write(index, value);
+                let value = self.buf().assume_init_clone(index);
+                iter.buf_mut().write(index, value);
+                *iter.end_mut() += 1;
             }
-            iter.buf.len += 1;
         }
         iter
     }
@@ -78,12 +110,14 @@ impl<T, const N: usize> Iterator for IntoIter<T, N> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.len() == 0 {
+        if self.start() == self.end() {
             return None;
         }
-        let index = self.index;
-        self.index += 1;
-        let value = unsafe { self.buf.buf.assume_init_read(index) };
+        let index = self.start();
+        unsafe {
+            *self.start_mut() += 1;
+        }
+        let value = unsafe { self.buf().assume_init_read(index) };
         Some(value)
     }
 
@@ -98,22 +132,21 @@ impl<T, const N: usize> Iterator for IntoIter<T, N> {
 
     fn nth(&mut self, n: usize) -> Option<Self::Item> {
         if n >= self.len() {
-            let to_drop = self.index..self.buf.len;
-            self.index = self.buf.len;
+            let to_drop = self.alive();
             unsafe {
-                self.buf.buf.partial_drop(to_drop);
+                *self.start_mut() = self.end();
+                self.buf_mut().partial_drop(to_drop);
             }
-            None
-        } else {
-            let index = self.index + n;
-            let to_drop = self.index..index;
-            self.index = index + 1;
-            unsafe {
-                self.buf.buf.partial_drop(to_drop);
-            }
-            let value = unsafe { self.buf.buf.assume_init_read(index) };
-            Some(value)
+            return None;
         }
+        let index = self.start() + n;
+        let to_drop = self.start()..index;
+        unsafe {
+            *self.start_mut() = index + 1;
+            self.buf_mut().partial_drop(to_drop);
+        }
+        let value = unsafe { self.buf().assume_init_read(index) };
+        Some(value)
     }
 
     fn last(mut self) -> Option<Self::Item> {
@@ -125,10 +158,12 @@ impl<T, const N: usize> Iterator for IntoIter<T, N> {
         F: FnMut(B, Self::Item) -> B,
     {
         let mut acc = init;
-        while self.len() != 0 {
-            let index = self.index;
-            self.index += 1;
-            let value = unsafe { self.buf.buf.assume_init_read(index) };
+        while self.start() != self.end() {
+            let index = self.start();
+            unsafe {
+                *self.start_mut() += 1;
+            }
+            let value = unsafe { self.buf().assume_init_read(index) };
             acc = f(acc, value);
         }
         acc
@@ -137,39 +172,40 @@ impl<T, const N: usize> Iterator for IntoIter<T, N> {
 
 impl<T, const N: usize> ExactSizeIterator for IntoIter<T, N> {
     fn len(&self) -> usize {
-        self.buf.len - self.index
+        self.end() - self.start()
     }
 }
 
 impl<T, const N: usize> DoubleEndedIterator for IntoIter<T, N> {
     fn next_back(&mut self) -> Option<Self::Item> {
-        if self.len() == 0 {
+        if self.start() == self.end() {
             return None;
         }
-        self.buf.len -= 1;
-        let index = self.buf.len;
-        let value = unsafe { self.buf.buf.assume_init_read(index) };
+        unsafe {
+            *self.end_mut() -= 1;
+        }
+        let index = self.end();
+        let value = unsafe { self.buf().assume_init_read(index) };
         Some(value)
     }
 
     fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
         if n >= self.len() {
-            let to_drop = self.index..self.buf.len;
-            self.buf.len = self.index;
+            let to_drop = self.alive();
             unsafe {
-                self.buf.buf.partial_drop(to_drop);
+                *self.end_mut() = self.start();
+                self.buf_mut().partial_drop(to_drop);
             }
-            None
-        } else {
-            let to_drop = (self.buf.len - n)..self.buf.len;
-            let index = to_drop.start - 1;
-            self.buf.len = index;
-            unsafe {
-                self.buf.buf.partial_drop(to_drop);
-            }
-            let value = unsafe { self.buf.buf.assume_init_read(index) };
-            Some(value)
+            return None;
         }
+        let to_drop = (self.end() - n)..self.end();
+        let index = to_drop.start - 1;
+        unsafe {
+            *self.end_mut() = index;
+            self.buf_mut().partial_drop(to_drop);
+        }
+        let value = unsafe { self.buf().assume_init_read(index) };
+        Some(value)
     }
 
     fn rfold<B, F>(mut self, init: B, mut f: F) -> B
@@ -177,10 +213,12 @@ impl<T, const N: usize> DoubleEndedIterator for IntoIter<T, N> {
         F: FnMut(B, Self::Item) -> B,
     {
         let mut acc = init;
-        while self.len() != 0 {
-            self.buf.len -= 1;
-            let index = self.buf.len;
-            let value = unsafe { self.buf.buf.assume_init_read(index) };
+        while self.start() != self.end() {
+            unsafe {
+                *self.end_mut() -= 1;
+            }
+            let index = self.end();
+            let value = unsafe { self.buf().assume_init_read(index) };
             acc = f(acc, value);
         }
         acc
@@ -191,9 +229,9 @@ impl<T, const N: usize> FusedIterator for IntoIter<T, N> {}
 
 impl<T, const N: usize> Drop for IntoIter<T, N> {
     fn drop(&mut self) {
-        let to_drop = self.index..self.buf.len;
+        let to_drop = self.alive();
         unsafe {
-            self.buf.buf.partial_drop(to_drop);
+            self.buf_mut().partial_drop(to_drop);
         }
     }
 }
